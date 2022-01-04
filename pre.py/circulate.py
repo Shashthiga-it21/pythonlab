59b88dbb47c12b50d2a9f10e2acb3dd81774c52c
shashthiga-it21 <shashthiga.it21@gmail.com>
2022-01-04 18:18:25 +0000

    commitmsg

---
+++ pre.py/circulate.py
@@ -0,0 +1,53 @@
+a=10
+b=20
+c=30
+d=40
+temp=a
+a=b
+b=c
+c=d
+d=temp
+print("###")
+print(a)
+print(b)
+print(c)
+print(d)
+
+a=10
+b=20
+c=30
+d=40
+for i in range(4):
+ temp=a
+ a=b
+ b=c
+ c=d
+ d=temp
+ print("###")
+ print(a)
+ print(b)
+ print(c)
+ print(d)
+
+
+list=[55,66,77,88,99,33,54,65,65,64]
+for i in range(10):
+ temp=i+1
+ i+2
+ i+3
+ i+4
+ i+5
+ i+6
+ i+7
+ i+8
+ i+9
+ i+10
+ i+10==temp
+ print("###")
+ print(list)
+
+
+ 
+
+
+
